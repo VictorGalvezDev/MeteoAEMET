340c23b383042cdf9ea5e758c133e78dcca5497a
VictorGalvezDev <nextDeveloper98@gmail.com>
2026-02-15 17:03:54 +0100

    import incorrecto

--- frontend/src/components/UI/metereologiaSkeleton.jsx
+++ frontend/src/components/UI/metereologiaSkeleton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./css/MetereologiaSkeleton.css";
+import "./css/metereologiaSkeleton.css";
 
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
